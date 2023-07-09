caa40620833b2f168b7e5082ec23f186d2cd258e
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-07-09 12:41:00 +0200

    initial movie-view

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,27 @@
+import "./movie-view.css";
+export const MovieView = ({ movieData, setSelectedMovieToNull }) => {
+  return (
+    <div>
+      <div>
+        <img src={movieData.imageUrl} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movieData.title}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movieData.genre.name}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movieData.description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movieData.director.name}</span>
+      </div>
+      <button onClick={setSelectedMovieToNull}>Close</button>
+    </div>
+  );
+};
